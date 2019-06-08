72d1b63a66fb143a95a96cab3daee01f8fc69a74
kgrogan87 <kgrogan87@mindspring.com>
2019-06-08 14:35:02 -0700

    Update stanShock.py

--- stanShock.py
+++ stanShock.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+'''
+    Copyright 2017 Kevin Grogan
+
+    This file is part of StanShock.
+
+    StanShock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    StanShock is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with StanShock.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 #necessary modules
 import numpy as np
