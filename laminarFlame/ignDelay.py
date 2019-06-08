63c2360bd6e73bef64aff18fe9c8e6c1c6d6eed9
kgrogan87 <kgrogan87@mindspring.com>
2019-06-08 14:39:09 -0700

    Update ignDelay.py

--- laminarFlame/ignDelay.py
+++ laminarFlame/ignDelay.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 21 14:16:41 2016
-These functions are used for the determination of the physical timescale and 
-lengthscales
-@author: kgrogan
-"""
+'''
+    Copyright 2017 Kevin Grogan
+    
+    This file is part of StanShock.
+    
+    StanShock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+    
+    StanShock is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public License
+    along with StanShock.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 import cantera as ct
 import numpy as np
