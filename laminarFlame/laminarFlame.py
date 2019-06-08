a313f7835452589764cf533a089c5ef2a96f24f2
kgrogan87 <kgrogan87@mindspring.com>
2019-06-08 14:39:42 -0700

    Update laminarFlame.py

--- laminarFlame/laminarFlame.py
+++ laminarFlame/laminarFlame.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-Created on Wed May 30 10:38:47 2018
-
-@author: kevin
-"""
+'''
+    Copyright 2017 Kevin Grogan
+    
+    This file is part of StanShock.
+    
+    StanShock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+    
+    StanShock is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    
+    You should have received a copy of the GNU Lesser General Public License
+    along with StanShock.  If not, see <https://www.gnu.org/licenses/>.
+'''
 import sys; sys.path.append('../')
 from stanShock import stanShock
 import numpy as np
