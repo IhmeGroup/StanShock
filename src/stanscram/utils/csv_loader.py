cc68da36dfd4846cfb66280e5aee02a37d2e8603
Tim E. Dawson <tim.dawson@cfd-research.com>
2025-02-21 23:41:48 -0600

    CSV loader Ruff pass.

--- src/stanscram/utils/csv_loader.py
+++ src/stanscram/utils/csv_loader.py
@@ -1,13 +1,17 @@
+from __future__ import annotations
+
+from pathlib import Path
+
 import cantera as ct
 import numpy as np
 
 
 def isFloat(value):
-    '''
+    """
     function isfloat
     ==========================================================================
     hacky python way to detect floats
-    '''
+    """
     try:
         float(value)
         return True
@@ -16,26 +20,30 @@ def isFloat(value):
 
 
 def loadData(fileName):
-    '''
+    """
     function loadData
     ==========================================================================
     This function loads the raw data contained in the csv file and initiates
     a list of dictionaries containg the data
         fileName: file name of csv data
         Return: list of dictionaries for each example
-    '''
+    """
     import csv
+
     rawData = []
-    with open(fileName) as csvFile:
+    with Path.open(fileName) as csvFile:
         reader = csv.DictReader(csvFile)
         for dictRow in reader:
-            cleanDictRow = {key: (float(dictRow[key]) if isFloat(dictRow[key]) else dictRow[key]) for key in dictRow}
+            cleanDictRow = {
+                key: (float(dictRow[key]) if isFloat(dictRow[key]) else dictRow[key])
+                for key in dictRow
+            }
             rawData.append(cleanDictRow)
     return rawData
 
 
 def getPressureData(fileName):
-    '''
+    """
     function getPressureData
     ==========================================================================
     This function returns the formatted pressure vs time data
@@ -44,7 +52,7 @@ def getPressureData(fileName):
         Outputs:
              t = time [s]
              p = pressure [Pa]
-    '''
+    """
     rawData = loadData(fileName)
     t = np.array([example["Time (s)"] for example in rawData])
     p = np.array([example["Pressure (atm)"] for example in rawData])
