b97aee64905746a748f65b0cd5e1ccf726e7f4d8
Matthew Bonanni <mbonanni001@gmail.com>
2025-05-01 14:08:34 -0700

    Clean up pyplot options

--- examples/hyshot_ii_jic/hyshot_ii_jic.py
+++ examples/hyshot_ii_jic/hyshot_ii_jic.py
@@ -13,28 +13,27 @@ from stanshock.physics.flamelet import FPVTable
 from stanshock.physics.jicf import JICModel
 from stanshock.processing.plot import XTDiagram
 
-plt.rcParams.update(
-    {
-        "text.usetex": True,
-        "font.family": "serif",
-        "font.serif": ["Computer Modern Roman"],
-    }
-)
-plt.rcParams["axes.xmargin"] = 0
-plt.rcParams["axes.ymargin"] = 0
-
 XSMALL_SIZE = 12
 SMALL_SIZE = 14
 MEDIUM_SIZE = 16
 BIGGER_SIZE = 18
 
-plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
-plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
-plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
-plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
-plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
-plt.rc("legend", fontsize=XSMALL_SIZE)  # legend fontsize
-plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rcParams.update(
+    {
+        "text.usetex": True,
+        "font.family": "serif",
+        "font.serif": ["Computer Modern Roman"],
+        "axes.xmargin": 0,
+        "axes.ymargin": 0,
+        "font.size": SMALL_SIZE,
+        "axes.titlesize": SMALL_SIZE,
+        "axes.labelsize": MEDIUM_SIZE,
+        "xtick.labelsize": SMALL_SIZE,
+        "ytick.labelsize": SMALL_SIZE,
+        "legend.fontsize": XSMALL_SIZE,
+        "figure.titlesize": BIGGER_SIZE,
+    }
+)
 
 # Plotting utilities
 scale = 1e3
