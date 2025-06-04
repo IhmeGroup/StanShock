0e586944f00f0ce371d2a460b61c12ef4d1ab646
Tim E. Dawson <tim.dawson@cfd-research.com>
2025-06-04 10:38:43 -0500

    [#38] Fully define Array and Index type aliases.

--- src/stanshock/system/backend.py
+++ src/stanshock/system/backend.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+from typing import Union
+
 import numpy as np
+from typing_extensions import TypeAlias
+
+__all__ = ["Array", "Index", "np"]
 
 # Define the Array type for use in type hints to make future changes easier
-Array = np.ndarray
+Array: TypeAlias = np.ndarray[tuple[int, ...], np.dtype[np.float64]]
+# Note: Must use Union instead of |, but only in Python <3.10 and only in TypeAlias
+Index: TypeAlias = Union[np.ndarray[tuple[int, ...], np.dtype[np.int64]], slice]
