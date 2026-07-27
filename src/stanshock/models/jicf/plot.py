a54fc49674ee87cb0d119f59c19619581bb0c3a0
Tim E. Dawson <tim.dawson@cfd-research.com>
2026-07-27 10:38:57 -0500

    [#101] Add centerline overlay to JICF plots.

--- src/stanshock/models/jicf/plot.py
+++ src/stanshock/models/jicf/plot.py
@@ -23,7 +23,13 @@ class PlotOptions:
     figsize: tuple[float, float]
 
 
-def _contour_plot(x: Array, y: Array, z: Array, opts: PlotOptions) -> None:
+def _contour_plot(
+    x: Array,
+    y: Array,
+    z: Array,
+    opts: PlotOptions,
+    extra: dict[str, tuple[Array, Array]] | None = None,
+) -> None:
     fig, ax = plt.subplots(figsize=opts.figsize)
 
     vmin = z.min()
@@ -33,6 +39,16 @@ def _contour_plot(x: Array, y: Array, z: Array, opts: PlotOptions) -> None:
     norm = Normalize(vmin=vmin, vmax=vmax)
     axcb = fig.colorbar(cs, fraction=0.046, pad=0.01).ax
     _cb = ColorbarBase(axcb, cmap=plt.get_cmap("inferno"), norm=norm)
+
+    if extra is not None:
+        # Overlay lines on the contour plot, adding a legend if there are more than one
+        multi = len(extra) > 1
+
+        for label, (xl, yl) in extra.items():
+            ax.plot(xl, yl, "-" if multi else "r--", label=label)
+
+        if multi:
+            ax.legend(loc="best")
 
     ax.set_aspect("equal", "box")
     ax.set_xlabel(opts.xlabel)
@@ -44,7 +60,7 @@ def _contour_plot(x: Array, y: Array, z: Array, opts: PlotOptions) -> None:
     plt.close(fig)
 
 
-def plot_jicf_flowfield(jicf: JICModel, plot_dir: Path = Path("plots/jicf")) -> None:
+def plot_jicf_flowfield(jicf: JICModel, plot_dir: Path = Path("figures/jicf")) -> None:
     """Generate contour plots of the JICF flow field"""
     plot_dir.mkdir(parents=True, exist_ok=True)
     x = jicf.x_3D_data
@@ -52,26 +68,47 @@ def plot_jicf_flowfield(jicf: JICModel, plot_dir: Path = Path("plots/jicf")) -> 
     z = jicf.z_3D_data
     Z = jicf.Z_3D_data
 
-    # Generate plot along injector centerlines
-    z_inj = jicf.analytic.z_inj
+    # Get injector location info for constraining the plots
+    x_min = 0.9 * jicf.x_inj + 0.1 * x[0]
+    ix_min = np.argmin(np.abs(x - x_min))
+    x_max = 0.6 * jicf.x_inj + 0.4 * x[-1]
+    ix_max = np.argmin(np.abs(x - x_max))
+    x = x[ix_min:ix_max]
+    Z = Z[ix_min:ix_max]
 
+    z_inj = jicf.analytic.z_inj
+    nx = len(x)
+    ix_inj = np.argmin(np.abs(x - jicf.x_inj))
+
+    # Compute jet centerline profiles
+    jicf.analytic.i_m = slice(None)
+    x_cl = np.linspace(0.0, x[-1] - jicf.x_inj, 1001)
+    y_cl = jicf.analytic.y_cl(x_cl[:, None])
+    x_cl += jicf.x_inj
+    y_cl[y_cl > y[-1]] = np.nan
+
+    # Generate plot along injector centerlines
     opts = PlotOptions(
         "Centerline Mixture Fraction",
         "x [m]",
         "y [m]",
         plot_dir / "Z_centerline.png",
-        (38.4, 3.2),
+        (38.4, 4.0),
     )
+
     for i in range(len(z_inj)):
         iz_inj = np.argmin(np.abs(z - z_inj[i]))
         for iJ in range(jicf.nJ):
             opts.title = f"Jet Centerline Mixture Fraction at z={z_inj[i]:.3f}, J={jicf.analytic.J[iJ]:.2f}"
-            opts.filename = plot_dir / f"Z_cl_z{i:02d}_J{iJ:02d}.png"
+            opts.filename = plot_dir / f"Z_z{i:02d}_J{iJ:02d}.png"
             _contour_plot(x, y, Z[:, :, iz_inj, iJ].T, opts)
 
+            opts.filename = plot_dir / f"Z_z{i:02d}_J{iJ:02d}_cl.png"
+            _contour_plot(
+                x, y, Z[:, :, iz_inj, iJ].T, opts, extra={"": (x_cl, y_cl[:, iJ])}
+            )
+
     # Generate plots in transverse slices
-    nx = len(x)
-    ix_inj = np.argmin(np.abs(x - jicf.x_inj))
     n_plot = 5
     di = (nx - ix_inj) // (n_plot - 1)
     if n_plot * di + ix_inj > nx - 1:
@@ -82,6 +119,8 @@ def plot_jicf_flowfield(jicf: JICModel, plot_dir: Path = Path("plots/jicf")) -> 
     for i in range(n_plot):
         ix = ix_inj + i * di
         for iJ in range(jicf.nJ):
-            opts.title = f"Mixture Fraction at x={x[ix]:.3f}, J={jicf.analytic.J[iJ]:.2f}"
+            opts.title = (
+                f"Mixture Fraction at x={x[ix]:.3f}, J={jicf.analytic.J[iJ]:.2f}"
+            )
             opts.filename = plot_dir / f"Z_x{i:02d}_J{iJ:02d}.png"
             _contour_plot(z, y, Z[ix, :, :, iJ], opts)
